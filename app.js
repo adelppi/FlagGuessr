3dfd39d98076f23c0e0f689d0643b68af7749c47
adelppi <adelppi.baig@gmail.com>
2023-03-05 20:25:11 +0900

    英語対応

--- app.js
+++ app.js
@@ -4,29 +4,51 @@ createApp({
     data() {
         return {
             countryInfos: [],
-            userInput: ""
+            userInput: "",
+            score: 0,
+            gameOver: false
         }
     },
     methods: {
         randCountry() {
             axios.get("./countries.json").then(response => (this.countryInfos = response.data[Math.floor(Math.random() * 250)]))
+        },
+        seeAnswer() {
+            this.gameOver = true
+        },
+        restart() {
+            this.gameOver = false
+            this.score = 0
+            this.userInput = ""
+            this.randCountry()
         }
     },
     computed: {
-        flagURL: function() {
+        flagURL: function () {
             return "https://flagcdn.com/" + `${this.countryInfos.alpha2}`.toLowerCase() + ".svg"
         },
-        answer: function() {
-            return this.countryInfos.countryJA
+        answer: function () {
+            console.log(this.language)
+            if (this.language == "ja") {
+                return this.countryInfos.countryJA
+            } else if (this.language == "en") {
+                return this.countryInfos.countryEN
+            } else {
+                return this.countryInfos.countryJA
+            }
+        },
+        language: function () {
+            return document.head.querySelector('[name=language]').content
         }
     },
-    mounted: function() {
+    mounted: function () {
         this.randCountry()
     },
     watch: {
-        userInput: function(userInput) {
-            if(userInput == this.answer){
+        userInput: function (userInput) {
+            if (userInput.toLowerCase() == this.answer.toLowerCase()) {
                 this.userInput = ""
+                this.score++
                 this.randCountry()
             }
         }
